9c01e0566abe3e1809b505c2147247f2db4dbe66
imad-el-hajjami <contact@imadelhajjami.com>
2023-08-25 09:27:08 -0700

    done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -P /root https://github.com/imad-el-hajjami/alx-low_level_programming/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+export LD_PRELOAD=/root/libgiga.so
 
